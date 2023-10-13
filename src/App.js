eeb4e04d7603ea021e493a2a6d8e87c27a21e1d2
Jose Copeti <jrcopeti@gmail.com>
2023-10-13 17:26:54 +0200

    only one item open at time

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import "./index.css";
 
 const faqs = [
   {
-    title: "Where are these chairs assembled?",
+    title: "What is the service provided?",
     text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
   },
   {
-    title: "How long do I have to return my chair?",
+    title: "How much is it?",
     text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
   },
   {
     title: "Do you ship to countries outside the EU?",
     text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+  {
+    title: "How many days to deliver?",
+    text: "Could you wave goodbye to sun? The sea, the stars, the waves, the tide?"
   },
 ];
 
@@ -25,28 +29,58 @@ export default function App() {
 }
 
 function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState();
+
   return (
     <div className="accordion">
       {data.map((el, i) => (
-        <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />
+        <AccordionItem
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+          title={el.title}
+          num={i}
+          key={el.title}
+        >
+          {el.text}
+        </AccordionItem>
       ))}
+
+      <AccordionItem
+        curOpen={curOpen}
+        onOpen={setCurOpen}
+        title="Why use 'children' as a prop? "
+        num={"extra"}
+        key={"extra"}
+      >
+        <p> Allows React developers to:</p>
+        <ul>
+          <li>Break up UI into components</li>
+          <li>Make components reusable</li>
+          <li>Place state efficiently</li>
+          <li>
+            Basically pass a piece of HTML instead of just text as content
+          </li>
+        </ul>
+      </AccordionItem>
     </div>
   );
 }
 
-function AccordionItem({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
+function AccordionItem({ num, title, curOpen, onOpen, children }) {
+  const isOpen = num === curOpen;
 
   function toggle() {
-    setIsOpen((isOpen) => !isOpen)
+    onOpen(isOpen ? null : num);
   }
 
   return (
-    <div className={`item ${isOpen? "open" : ""}`} onClick={toggle}>
-      <p className="number">{num < 9 ? `0${num + 1}` : num + 1 }</p>
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={toggle}>
+      <p className="number">
+        {typeof num === "string" ? num : num < 9 ? `0${num + 1}` : num + 1}
+      </p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
-     {isOpen && <div className="content-box">{text}</div>}
+      {isOpen && <div className="content-box">{children}</div>}
     </div>
   );
 }
